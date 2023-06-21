999153eeaf66b4b97f9a4344fc002423426d6e83
Pietromuresu <muresupietro66@gmail.com>
2023-06-21 18:28:11 +0200

    Seed Project type_id with random Type id

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator as Faker;
 
 class ProjectsTableSeeder extends Seeder
@@ -19,6 +20,7 @@ class ProjectsTableSeeder extends Seeder
         for($i = 0; $i < 10; $i++){
 
             $new_project = new Project();
+            $new_project->type_id = Type::inRandomOrder()->first()->id;
             $new_project->name = $faker->sentence(3);
             $new_project->slug = Project::generateSlug($new_project->name);
             $new_project->purpose = $faker->sentence(10);
